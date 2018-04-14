5366cf5235215a1952de61d5db4373aa155344bd
mzhang <mzhang@twitter.com>
2018-04-14 11:12:02 -0700

    create dist

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const fs = require('fs');
 const browserify = require('browserify');
 
-module.exports = browserify('./src/compiler.js').transform('babelify', { presets: ['stage-2'] });
+module.exports = browserify('./src/compiler.js')
+  .transform('babelify', { presets: ['stage-2'] })
+  .bundle()
+  .pipe(fs.createWriteStream('./dist/bundle.js'));
